06a9879a90d6ac450d9a933dfd76badfb4d56424
SentientSchnitzel <simondanielschneider@gmail.com>
2023-05-06 11:23:56 +0200

    cooler nowww

--- apps.js
+++ apps.js
@@ -1,3 +1,34 @@
+function scrollToSection(event, targetId) {
+    event.preventDefault();
+    const target = document.getElementById(targetId);
+    const windowHeight = window.innerHeight;
+    const yOffset = -windowHeight * 0.25; // Position section 25% from the top
+    const y = target.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    smoothScrollTo(y, 250); // 250 milliseconds duration
+}
+
+function smoothScrollTo(targetY, duration) {
+    const startY = window.scrollY;
+    const difference = targetY - startY;
+    const startTime = performance.now();
+
+    function step(currentTime) {
+        const elapsed = currentTime - startTime;
+        const progress = Math.min(elapsed / duration, 1);
+        const easeProgress = progress < 0.5 ? 2 * progress * progress : (4 - 2 * progress) * progress - 1;
+        window.scrollTo(0, startY + difference * easeProgress);
+
+        if (elapsed < duration) {
+            requestAnimationFrame(step);
+        }
+    }
+
+    requestAnimationFrame(step);
+}
+
+
+
+
 const observer = new IntersectionObserver(entries => {
     entries.forEach((entry) => {
         console.log("heyo")
